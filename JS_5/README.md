17b1f6ee5b8cf4ed7ad960cacfd48f6cde6db507
Kseniya Parkhamchuk <ksusha.rr@yandex.ru>
2020-04-06 21:25:13 +0300

    homework JS 5

---
+++ JS_5/README.md
@@ -0,0 +1,11 @@
+1. npm install
+
+Node 8.x
+Webpack 4.x
+
+2. npm start - to run local server
+
+3. npm run build - to create build
+
+
+branck Lesson 1 new TEXT
---
+++ JS_5/src/app/app.js
@@ -0,0 +1,18 @@
+import './app.scss';
+
+//import './components/Lesson_JS_1'
+//import './components/Lesson_JS_2'
+
+//import './components/Lesson_JS_3'
+
+//import './components/Lesson_JS_4'
+//import './components/Lesson_JS_5'
+import './components/Homework_JS_5'
+
+import $ from 'jquery'
+
+window.jQuery = $;
+
+
+
+
